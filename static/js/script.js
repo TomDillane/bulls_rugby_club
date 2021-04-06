bfa2c796b25934990d323e04179a3377438fa96c
TomDillane <tommy.c.dillane@gmail.com>
2021-04-06 21:41:45 +0000

    Feat: Added sign up form logic to display based on selection

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,17 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.sidenav').sidenav();
-  });
+    $('select').formSelect();
+});
+
+function noshowpos(){
+  document.getElementById('position-select').style.display ='none';
+  document.getElementById('gender-select').style.display ='none';
+}
+function showpos(){
+  document.getElementById('position-select').style.display = 'block';
+  document.getElementById('gender-select').style.display = 'block';
+}
+
+
+   
+
