8b3eeb3a49a5c9df35929af0a16857e2ccc891ae
TomDillane <tommy.c.dillane@gmail.com>
2021-04-16 20:02:47 +0000

    Doc: added and update comments in js and css

--- static/js/script.js
+++ static/js/script.js
@@ -1,22 +1,24 @@
+// materialize initialization 
 $(document).ready(function () {
     $('.sidenav').sidenav();
     $('select').formSelect();
     $('.datepicker').datepicker();
 });
 
+// condition on signup form to show player relevant fields if selected
 function check() {
     let member = document.getElementById("member-selector").value;
-    if(member == "player") {
+    if (member == "player") {
         document.getElementById("position-select").style.display = 'block';
-    document.getElementById("gender-select").style.display = 'block';
-    document.getElementById("pos-selector").required = true;
-    document.getElementById("gender-select").required = true;
+        document.getElementById("gender-select").style.display = 'block';
+        document.getElementById("pos-selector").required = true;
+        document.getElementById("gender-select").required = true;
     }
     else {
         document.getElementById("position-select").style.display = 'none';
-    document.getElementById("gender-select").style.display = 'none';
-    document.getElementById("pos-selector").required = false;
-    document.getElementById("gender-select").required = false;
+        document.getElementById("gender-select").style.display = 'none';
+        document.getElementById("pos-selector").required = false;
+        document.getElementById("gender-select").required = false;
     }
 }
 
