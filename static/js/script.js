3cdc45ff24a5467c3bd38e28285c520b197baab3
TomDillane <tommy.c.dillane@gmail.com>
2021-05-09 13:56:05 +0000

    Maint: removed commented out code

--- static/js/script.js
+++ static/js/script.js
@@ -38,26 +38,3 @@ function check1() {
     }
 }
 
-
-/*
-function noshowpos() {
-    document.getElementById('position-select').style.display = 'none';
-    document.getElementById('gender-select').style.display = 'none';
-}
-function showpos() {
-    document.getElementById('position-select').style.display = 'block';
-    document.getElementById('gender-select').style.display = 'block';
-}
-
-
-
-$('#member-selector').change(function () {
-    if(this.checked) {
-        $('#gender-selector').prop('required', true);
-        $('#pos-selector').prop('required', true);
-    } else {
-        $('#gender-selector').prop('required', false);
-        $('#pos-selector').prop('required', false);
-    }
-});
-*/
